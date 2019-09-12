4124e2e7cfa404d04644228734e988b8e75678c9
Maddy <ms9642@nyu.edu>
2019-09-12 15:34:57 -0400

    Movement of player and initial walls

---
+++ Assets/Scripts/mainBehavior.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown (KeyCode.UpArrow)) {
+            this.GetComponent<Transform> ().Translate (new Vector3 (0, 0.2f));		
+        }
+        if (Input.GetKeyDown (KeyCode.DownArrow)) {
+            this.GetComponent<Transform> ().Translate (new Vector3 (0, -0.2f));	
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            this.GetComponent<Transform> ().Translate (new Vector3 (0.2f, 0)); 
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            this.GetComponent<Transform> ().Translate (new Vector3 (-0.2f, 0));
+        }
+        
+    }
+}
