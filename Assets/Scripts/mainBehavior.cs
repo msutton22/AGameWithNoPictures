c21d70547ad469fa5fe3006aeb850e2e56ab9e0b
Maddy <ms9642@nyu.edu>
2019-09-16 17:35:37 -0400

    text popups added

--- Assets/Scripts/mainBehavior.cs
+++ Assets/Scripts/mainBehavior.cs
@@ -9,17 +9,63 @@ public class mainBehavior : MonoBehaviour
     public AudioSource sound1;
     public AudioSource sound2;
     public AudioSource sound3;
-
     public AudioSource sound4;
+
+    public float timer = 2f;
+    private GameObject popup;
+    
+    public GameObject popup1;
+    public GameObject popup2;
+    public GameObject popup3;
+    public GameObject popup4;
+
+    private GameObject text;
+    private GameObject text2;
+    private GameObject text3;
+    private GameObject text4;
 
     // Start is called before the first frame update
     void Start()
     {
+      
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (text != null)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0)
+            {
+                text.SetActive(false);
+            }
+        }
+        if (text2 != null)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0)
+            {
+                text2.SetActive(false);
+            }
+        }
+        if (text3 != null)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0)
+            {
+                text3.SetActive(false);
+            }
+        }
+        if (text4 != null)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0)
+            {
+                text4.SetActive(false);
+            }
+        }
+        
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             this.GetComponent<Transform>().Translate(new Vector3(0, 0.2f));
@@ -50,14 +96,16 @@ public class mainBehavior : MonoBehaviour
         }
         if (collision.gameObject.tag.Equals("wall"))
         {
+            timer = 2f;
             ChooseMusic();
+            ChoosePopup();
         }
 
     }
 
     void ChooseMusic()
     {
-        var randomMusic = Random.Range(0, 3);
+        var randomMusic = Random.Range(0, 4);
 
         switch (randomMusic)
         {
@@ -72,6 +120,33 @@ public class mainBehavior : MonoBehaviour
                 break;
             case 3:
                 sound4.Play();
+                break;
+        }
+
+    }
+    
+    void ChoosePopup()
+    {
+        var randomPopup = Random.Range(0, 4);
+        GameObject canvas = GameObject.Find("Canvas");
+
+        switch (randomPopup)
+        {
+            case 0:
+                text = Instantiate(popup1, new Vector3(300,200,0), Quaternion.identity);
+                text.transform.SetParent(canvas.transform);
+                break;
+            case 1:
+                text2 = Instantiate(popup2, new Vector3(900,200,0), Quaternion.identity);
+                text2.transform.SetParent(canvas.transform);
+                break;
+            case 2:
+                text3 = Instantiate(popup3, new Vector3(200,600,0), Quaternion.identity);
+                text3.transform.SetParent(canvas.transform);
+                break;
+            case 3:
+                text4 = Instantiate(popup4, new Vector3(700,600,0), Quaternion.identity);
+                text4.transform.SetParent(canvas.transform);
                 break;
         }
 
