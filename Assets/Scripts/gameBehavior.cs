edd6b679446d6bf2473c103d7605f9457da7c655
Maddy <ms9642@nyu.edu>
2019-09-15 21:22:11 -0400

    added timer and lose condition

---
+++ Assets/Scripts/gameBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class gameBehavior : MonoBehaviour
+{
+    public float timer = 90f;
+    public GameObject timerText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        timerText.gameObject.GetComponent<Text>().text = ("Time Left: " + (int)timer);
+
+        if (timer <= 0)
+        {
+            SceneManager.LoadScene (2);
+        }
+    }
+}
